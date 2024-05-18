29f977ee6ec2b0abd33f5812d990bc01c4c4f301
Sai Jayakeerthi <jaykeerti123@gmail.com>
2024-05-18 23:32:33 +0530

    Quiz App intital version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import {Amplify} from 'aws-amplify';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import Quiz from './Quiz';
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        {({ signOut }) => (
+          <main>
+            <header className='App-header'>
+              <Quiz />
+              <button 
+                onClick={signOut} 
+                style={{ 
+                  margin: '20px', 
+                  fontSize: '0.8rem', 
+                  padding: '5px 10px', 
+                  marginTop: '20px'
+                }}
+              >
+                Sign Out
+              </button>
+            </header>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
